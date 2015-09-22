485f64eaf422e8edef3fd621deaf3475ecdd555f
Daniel <fradadalape@hotmail.com>
2015-09-22 16:18:01 -0600

    Rename Controller Directorio

--- app/Http/Controllers/DirectorioController.php
+++ app/Http/Controllers/DirectorioController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class Directorio extends Controller
+class DirectorioController extends Controller
 {
     /**
      * Display a listing of the resource.
